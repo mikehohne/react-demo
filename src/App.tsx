613e856727928b53abe17f82238ddf68c0af9c87
Michael Hohne <michaelh@MHohne-MBP15-2019.local>
2019-10-17 19:49:59 -0600

    master push.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC } from 'react';
+import Avatar from './components/Avatar';
+import axios from 'axios';
+import "./App.css";
 
-const App: React.FC = () => {
+interface HeaderProps {
+  name: string;
+}
+
+interface FooterProps {
+  companyName: string;
+}
+
+interface AppProps {
+  content: string;
+  name: string;
+}
+
+const App: FC = () => {
+  const content = "This is content";
+  const name = "Mike";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name={name} />
+      <Content content={content} />
+      <Footer companyName={'Schedulicity'} />
     </div>
   );
+};
+
+const Header: React.FC<HeaderProps> = (props) => {
+  return (
+    <div style={{ backgroundColor: 'yellow', height: '10vh'}}>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', paddingTop: '3vh' }}>
+        <h1 style={{ margin: '0', marginRight: '4px'}}>Hello {props.name}</h1> 
+        <Avatar />
+      </div>
+    </div>
+  );
+};
+
+const Content: React.FC<any> = (props: any) => <div className="content">{props.content}</div>;
+
+class Footer extends React.Component<FooterProps> {
+
+  private name: string;
+  
+  constructor(props: FooterProps) {
+    super(props);
+    this.name = props.companyName;
+  }
+
+  state = {
+    counter: 0
+  };
+
+  componentDidMount = async () => {
+    const result = await this.fetchTodo();
+    console.log(result.data);
+  }
+
+
+  fetchTodo = () => {
+    try {
+      return axios.get('https://jsonplaceholder.typicode.com/todos/15');
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  increment = () => {
+    this.setState({
+      counter: this.state.counter + 1
+    })
+  }
+
+  decrement = () => {
+    this.setState({
+      counter: this.state.counter - 1
+    })
+  }
+
+  render() {
+    let decrementButton;
+    if (this.state.counter > 0) {
+      decrementButton = <button onClick={this.decrement}>Decrement</button>;
+    }
+    return (
+      <div className="App-footer">
+        <h1>Counter: {this.state.counter}</h1>
+        <button onClick={this.increment}>Increment</button>
+        {decrementButton}
+        <h6>{this.name}</h6>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Avatar.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import logo from '../logo.svg';
+
+const Avatar: React.FC<any> = () => {
+    return <img src={logo} alt="logo" width="40px" height="40px" />
+}
+
+export default Avatar;
